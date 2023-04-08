aacea02d2e6513f62e84d04d84d15bcdaa44fb7d
AlinaBasyrova <129521982+AlinaBasyrova@users.noreply.github.com>
2023-04-08 10:47:06 +0500

    .

--- PlatformerGame2023/Game1.cs
+++ PlatformerGame2023/Game1.cs
@@ -8,6 +8,18 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    Texture2D _texture;
+    Vector2 position = new Vector2(0, 10);
+    float speed = 2f;
+    
+    int currentTime = 0; // сколько времени прошло
+    int period = 50; // частота обновления в миллисекундах
+
+    int frameWidth = 32;
+    int frameHeight = 32;
+    Point currentFrame = new Point(0, 0);
+    Point spriteSize = new Point(1, 10);
+
 
     public Game1()
     {
@@ -18,16 +30,19 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _texture = Content.Load<Texture2D>("2d/ChikBoy_run");
 
         // TODO: use this.Content to load your game content here
+    }
+
+    protected override void UnloadContent()
+    {
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,6 +50,20 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        currentTime += gameTime.ElapsedGameTime.Milliseconds;
+        if (currentTime > period)
+        {
+            currentTime -= period;
+ 
+            position.X += speed;
+
+            ++currentFrame.Y;
+            if (currentFrame.Y >= spriteSize.Y)
+            {
+                currentFrame.Y = 0;
+            }
+        }
 
         // TODO: Add your update logic here
 
@@ -45,6 +74,17 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
+        // отрисовка спрайта
+        _spriteBatch.Begin();
+        
+        _spriteBatch.Draw(_texture, position,
+            new Rectangle(currentFrame.X * frameWidth,
+                currentFrame.Y * frameHeight,
+                frameWidth, frameHeight),
+            Color.White, 0, Vector2.Zero,
+            1, SpriteEffects.None, 0);
+        
+        _spriteBatch.End();
         // TODO: Add your drawing code here
 
         base.Draw(gameTime);
